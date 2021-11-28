41048b926c3476f266bb806414447d01d4300cf0
CleverUser123 <94817071+CleverUser123@users.noreply.github.com>
2021-11-27 23:31:08 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TechAndStuff'
+copyright = '2021, CleverUser123'
+author = 'CleverUser123'
 
 release = '0.1'
 version = '0.1.0'
